1cf9e09b30ce6f70576fb18cbd2f1b19837253b9
Kaylee Spindler <gl13ch@Kaylees-MacBook-Air.local>
2025-04-19 16:51:49 -0500

    connected to dndapi

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 $(() => {
-    
+    $.ajax({
+        url:'https://www.dnd5eapi.co/api/2014/spells/acid-arrow'
+    }).then(
+        (data)=>{
+            console.log(data)
+        },
+        ()=>{
+            console.log('Bad Request')
+        }
+    )
 })
-
-console.log('loaded')
