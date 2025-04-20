3ea58453db995e8aa8266abb1bd61572c7e5003c
Kaylee Spindler <gl13ch@Kaylees-MacBook-Air.local>
2025-04-19 21:11:28 -0500

    added all needed buttons set up some css

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 $(() => {
-    $.ajax({
-        url:'https://www.dnd5eapi.co/api/2014/spells/acid-arrow'
-    }).then(
-        (data)=>{
-            console.log(data)
-        },
-        ()=>{
-            console.log('Bad Request')
-        }
-    )
+    const getData = (data) => {
+        $.ajax({
+            url:`https://www.dnd5eapi.co/api/2014/${data}`
+        }).then(
+            (data)=>{
+                // console.log(data)
+                for (let i = 0; i < data.count; i++) {
+                    console.log(data.results[i])
+                }
+            },
+            ()=>{
+                console.log('Bad Request')
+            }
+        )
+    }
+
+    $('.info').on('click', (e)=>{
+        const info = $(e.currentTarget).attr('value')
+        getData(info)
+    })
 })
